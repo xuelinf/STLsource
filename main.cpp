f3bd4771d7889e304d97e585a8a0fb02da62d4c8
xuelinf <snowfacewind@gmail.com>
2015-12-03 17:21:18 +0800

    self_alloc 自己实现一个配置器。

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//
+// Created by xuelinf on 2015/12/3.
+//
+
+#include <iostream>
+#include <vector>
+#include "self_alloc.h"
+
+using namespace std;
+
+int main(){
+    vector<int> ia= {0,1,2,3,4};
+    unsigned int i;
+
+    vector<int, XLF::allocator<int> > iv{1,2,3,4,5};
+
+
+    for (i = 0; i < iv.size(); i++){
+        cout << iv[i] << " ";
+    }
+    cout <<endl;
+    return 0;
+}
